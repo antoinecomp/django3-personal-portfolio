a359eb84ffedb95c81cdd46ba1748e83732d643b
Antoine Compagnie <antoine.compagnie@gmail.com>
2021-02-26 17:59:17 +0100

    added more projects

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,6 +6,12 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+    TYPES = (
+        ('Webapp', 'Webapp'),
+        ('DS', 'Data Science'),
+        ('Courses', 'Courses'),
+    )
+    type = models.CharField(max_length=7, default='None', choices=TYPES)
 
     def __str__(self):
         return self.title
